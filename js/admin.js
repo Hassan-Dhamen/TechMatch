cc756bb2eb76f914355d3ad1054c4eec05c94798
iMr-MBA <moh.aalkhalifah2@gmail.com>
2026-04-14 23:40:39 +0300

    Add function to dynamically create spec input fields

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,13 @@
+function addSpec(){
+  let container = document.getElementById("specs-container")
 
+  let div = document.createElement("div")
+  div.classList.add("spec-row")
+
+  div.innerHTML = `
+    <input type="text" placeholder="Spec Name">
+    <input type="text" placeholder="Value">
+  `
+
+  container.appendChild(div)
+}
